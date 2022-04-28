c9da1a04810a2069a0d346a01e85f0f753f3dba2
acosson <antho.cosson@gmail.com>
2022-04-28 13:51:46 +0200

    vrai faux avec databinding

--- Module 4 Envoyer des donnees a lIHM/hello_world/app/src/main/java/fr/wololo/hello_world/MainActivity.kt
+++ Module 4 Envoyer des donnees a lIHM/hello_world/app/src/main/java/fr/wololo/hello_world/MainActivity.kt
@@ -4,26 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 import com.google.android.material.switchmaterial.SwitchMaterial
+import fr.wololo.hello_world.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val tv = findViewById<TextView>(R.id.tv_blabla)
-        val sw = findViewById<SwitchMaterial>(R.id.reponse_ca_va)
-        val et = findViewById<EditText>(R.id.et_reponse_courriel)
-        val sb = findViewById<SeekBar>(R.id.sb_niveau)
-        val bouton1 = findViewById<Button>(R.id.btn_1)
+        val vb = ActivityMainBinding.inflate(layoutInflater)
 
-        bouton1.setOnClickListener {
+        vb.btn1?.setOnClickListener {
             Toast.makeText(this,"Wololotitude",Toast.LENGTH_LONG).show();
         }
 
-        tv.text = "Wololo"
-
+        setContentView(vb.root)
     }
-
-
 }
