8292a2edecb3e76d81e97b9aad2ec7cb21e15912
acosson <antho.cosson@gmail.com>
2022-05-02 10:00:14 +0200

    Utilisation du ViewModel sans LiveData

---
+++ BestFinger/app/src/main/java/fr/wololo/bestfinger/MainActivityViewModel.kt
@@ -0,0 +1,40 @@
+package fr.wololo.bestfinger
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel()
+{
+    var nbLance = 0;
+    var nbLanceGauche = 0;
+    var nbLanceDroite = 0;
+    var scoreGauche = 0;
+    var scoreDroite = 0;
+    var img = R.drawable.d1;
+
+    private fun lance():Int
+    {
+        nbLance++
+        var valeur =  (1..6).random()
+        img = when(valeur) {
+            1 -> R.drawable.d1
+            2 -> R.drawable.d2
+            3 -> R.drawable.d3
+            4 -> R.drawable.d4
+            5 -> R.drawable.d5
+            else -> R.drawable.d6
+        }
+        return valeur
+    }
+
+    fun clicGauche()
+    {
+        nbLanceGauche++
+        scoreGauche += lance()
+    }
+
+    fun clicDroite()
+    {
+        nbLanceDroite++
+        scoreDroite += lance()
+    }
+}
